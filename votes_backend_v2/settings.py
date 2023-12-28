d00bbb290ab3c72f2a2d4f52b68132f42a59a0c2
kvorange <smartstiller@gmail.com>
2023-12-28 23:24:17 +0500

    disable csrf

--- votes_backend_v2/settings.py
+++ votes_backend_v2/settings.py
@@ -130,7 +130,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-    )
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+
+    ),
 }
 
 LANGUAGE_CODE = 'ru-ru'
