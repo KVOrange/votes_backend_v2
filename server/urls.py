d2c7ee4dcec1a6cc7e2a95ca53caa50b2bc36914
kvorange <smartstiller@gmail.com>
2023-12-28 22:13:00 +0500

    add configuration to admin page

--- server/urls.py
+++ server/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from config import config
 
 urlpatterns = [
     path(f'{config.CONFIG_FILE}', admin.site.urls),
