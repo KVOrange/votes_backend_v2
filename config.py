3169e63366ba714dfae2b3345124717e6afcf3b3
kvorange <smartstiller@gmail.com>
2023-12-28 22:10:29 +0500

    add configuration to admin page

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ config.read(CONFIG_FILE)
 
 SERVER_IP = config.get('SERVER', 'IP')
 DOMEN = config.get('SERVER', 'DOMEN')
+ADMIN_PATH = config.get('SERVER', 'ADMIN_PATH')
 
 DATABASE_HOST = config.get('DATABASE', 'HOST')
 DATABASE_USER = config.get('DATABASE', 'USER')
--- server/urls.py
+++ server/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{config.CONFIG_FILE}', admin.site.urls),
     path('system/', include('server.apps.system.urls')),
     path('api/', include('server.apps.votes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
